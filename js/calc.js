8490799c32ff098eca395e5395ef48ecd3e75fab
JonasBG <jonasbg05@gmail.com>
2015-10-18 19:10:28 -0500

    first commit

---
+++ js/calc.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	document.getElementById("popup").onclick = function(){ 
+		window.open('factor.html','Calculadora','width=443,height=397,menubar=0,scrollbars=0,toolbar=0,location=0,directories=0,resizable=0')
+	}
+
+}
