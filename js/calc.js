d724242399860c0be5270dfb1a4a891cea0c2df9
JonasBG <jonasbg05@gmail.com>
2015-10-26 22:56:31 -0500

    centrando index

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 	document.getElementById("popup").onclick = function(){ 
-		window.open('factor.html','Calculadora','width=443,height=397,menubar=no,scrollbars=no,toolbar=no,location=no,directories=no,resizable=no')
+		window.open('factor.html','_blank', 'width=443, height=397, menubar=no, scrollbars=no, toolbar=no, location=no, directories=no, resizable=no, titlebar=no')
 	}
 
 } 
