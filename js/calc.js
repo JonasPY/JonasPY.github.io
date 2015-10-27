8e1ed1c0f49dbbf52e671f5db4875c85d663a39d
JonasBG <jonasbg05@gmail.com>
2015-10-26 21:57:31 -0500

    modificando popup

--- js/calc.js
+++ js/calc.js
@@ -3,4 +3,14 @@ window.onload = function() {
 		window.open('factor.html','Calculadora','width=443,height=397,menubar=no,scrollbars=no,toolbar=no,location=no,directories=no,resizable=no')
 	}
 
-}
+} 
+
+
+// $(document).on('ready', function(){ //cuando el documento este listo
+
+// 		$('#popup').click(function(e){
+// 			e.preventDefault();
+// 			$(this).load('./factor.html');
+// 		});
+
+// });
