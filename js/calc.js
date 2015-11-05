4719bbe637968960b63c04bccf41550408c052bc
JonasBG <jonasbg05@gmail.com>
2015-11-04 23:45:13 -0500

    cambiando tabla de factores por actual nov

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,9 @@
 window.onload = function() {
 	document.getElementById("popup").onclick = function(){ 
-		window.open('factor.html','_blank', 'width=443, height=397, menubar=no, scrollbars=no, toolbar=no, location=no, directories=no, resizable=no, titlebar=no')
+		window.open('factor.html','_blank','width=443,height=397,menubar=no,scrollbars=no,toolbar=no,location=no,directories=no,resizable=0,titlebar=no')
 	}
 
-} 
+}
 
 
 // $(document).on('ready', function(){ //cuando el documento este listo
